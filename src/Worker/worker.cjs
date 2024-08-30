80814187ce7df743bd45067e62cf5429d3179e38
sumit.singh <sumit.singh@powerweave.com>
2024-08-30 11:49:00 +0530

    .

--- src/Worker/worker.cjs
+++ src/Worker/worker.cjs
@@ -12,4 +12,16 @@ parentPort.postMessage(counter);
 
 This file has been saved as extension as .cjs, means CommonJS in which we can user require() method.
 
+How to use Multithreading with "worker threads" in Node.js?
+https://www.youtube.com/watch?v=MuwJJrfIfsU&list=PL5Lsd0YA4OMGN86vWiW7O52izu-cTxcS3
+code for above video
+https://www.digitalocean.com/community/tutorials/how-to-use-multithreading-in-node-js
+
+for practice
+https://www.digitalocean.com/community/tutorials/how-to-handle-cpu-bound-tasks-with-web-workers
+
+https://askubuntu.com/questions/741298/how-to-get-datetime-using-curl-command
+cmd
+curl -v https://google.com/
+
 */
